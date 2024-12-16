3161152eae61b92580c4f84b45d2ada402fd2a8e
Abdul Ghofur <abdulghofur.me@gmail.com>
2024-12-16 07:52:25 +0700

    fix typo

--- app/api/v1/categories/model.js
+++ app/api/v1/categories/model.js
@@ -6,7 +6,7 @@ const categorySchema = Schema(
     name: {
       type: String,
       minLength: [3, "Panjang nama kategori minimal 3 karakter"],
-      maxLength: [20, "Panjang nama kategori maksimal 3 karakter"],
+      maxLength: [20, "Panjang nama kategori maksimal 20 karakter"],
       required: [true, "Nam kategori harus diisi"],
     },
   },
